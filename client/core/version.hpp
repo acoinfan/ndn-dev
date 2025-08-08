1f30344bfa8f974330005222de6ab7d4d89708d2
a_coin_fan <1770393635@qq.com>
2025-08-08 17:14:33 +0800

    async-consumer v0

---
+++ client/core/version.hpp
@@ -0,0 +1,12 @@
+#ifndef NDN_TOOLS_CORE_VERSION_HPP
+#define NDN_TOOLS_CORE_VERSION_HPP
+
+namespace ndn {
+namespace tools {
+
+extern const char* const VERSION;
+
+} // namespace tools
+} // namespace ndn
+
+#endif // NDN_TOOLS_CORE_VERSION_HPP
